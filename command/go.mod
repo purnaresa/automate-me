b7be1e2f5325ebbc1ac191025637af4238924257
purnaresa <me@purnaresa.co>
2021-07-07 00:00:22 +0700

    ready to use

--- command/go.mod
+++ command/go.mod
@@ -2,6 +2,7 @@ require (
 	github.com/aws/aws-lambda-go v1.23.0
 	github.com/aws/aws-sdk-go v1.38.56
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/sirupsen/logrus v1.8.1
 )
 
 module hello-world
--- command/main.go
+++ command/main.go
@@ -5,7 +5,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -25,6 +26,18 @@ type CommandInput struct {
 	Statement string `json:"Statement"`
 }
 
+type DbSecret struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Engine   string `json:"engine"`
+	Host     string `json:"host"`
+	Port     int64  `json:"port"`
+}
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+}
+
 func generateOutput(message string) (output string) {
 	commandOutput := CommandOutput{
 		Message: "command success",
@@ -35,15 +48,8 @@ func generateOutput(message string) (output string) {
 	return
 }
 
-type DbSecret struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Engine   string `json:"engine\"`
-	Host     string `json:"host"`
-	Port     int64  `json:"port"`
-}
-
 func getSecret(target, db string) (dbConn, dbEngine string) {
+	log.Debugln("getSecret start")
 	svc := secretsmanager.New(session.New())
 	input := &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String(target),
@@ -66,10 +72,12 @@ func getSecret(target, db string) (dbConn, dbEngine string) {
 		dbSecret.Port,
 		db)
 	dbEngine = dbSecret.Engine
+	log.WithField("secret", dbSecret.Host).Debugln("getSecret complete")
 	return
 }
 
 func executeStatement(dbConn, dbEngine, statement string) (err error) {
+	log.Debugln("executeStatement start")
 	db, err := sql.Open(dbEngine, dbConn)
 	if err != nil {
 		log.Println(err)
@@ -79,20 +87,53 @@ func executeStatement(dbConn, dbEngine, statement string) (err error) {
 		err = errExec
 		log.Println(err)
 	}
+
+	log.Debugln("executeStatement complete")
+	return
+}
+
+func parseBody(body []byte) (command CommandInput, err error) {
+	log.Debugln("parseBody start")
+	type RawBody struct {
+		Key    string `json:"key"`
+		Fields struct {
+			Customfield10156 struct {
+				Value string `json:"value"`
+			} `json:"customfield_10156"`
+			Customfield10160 struct {
+				Value string `json:"value"`
+			} `json:"customfield_10160"`
+			Customfield10157 string `json:"customfield_10157"`
+			// Summary          string `json:"summary"`
+		} `json:"fields"`
+	}
+
+	var raw RawBody
+
+	err = json.Unmarshal([]byte(body), &raw)
+	if err != nil {
+		log.Errorln(err)
+		return
+	}
+
+	command = CommandInput{
+		Target:    raw.Fields.Customfield10156.Value,
+		Db:        raw.Fields.Customfield10160.Value,
+		Statement: raw.Fields.Customfield10157,
+	}
+
+	log.WithField("body", command.Target).Debugln("parseBody complete")
 	return
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var command CommandInput
-	jsonErr := json.Unmarshal([]byte(request.Body), &command)
-	if jsonErr != nil {
-		return events.APIGatewayProxyResponse{}, errors.New(jsonErr.Error())
+	command, err := parseBody([]byte(request.Body))
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, errors.New(err.Error())
 	}
 
 	dbConn, dbEngine := getSecret(command.Target, command.Db)
-	log.Println(dbConn)
-	log.Println(dbEngine)
-	err := executeStatement(dbConn, dbEngine, command.Statement)
+	err = executeStatement(dbConn, dbEngine, command.Statement)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
